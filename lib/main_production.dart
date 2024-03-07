1bbf010173593df5a84391009878c25938a89801
ThanaaAdel <swaadl971@gmail.com>
2024-03-07 20:21:17 +0200

    flavars

---
+++ lib/main_production.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+import 'core/Routing/app_router.dart';
+import 'core/di/dependacy_injection.dart';
+import 'doctoria_app.dart';
+
+void main() {
+
+  setupGetIt();
+  runApp( DoctorIaApp(appRouter: AppRouter(),));
+}
+
+
